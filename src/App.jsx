c6a7424815bc20a981651bc3b4a16d4c5753e293
GeKtoRiX <gektor93@gmail.com>
2025-10-22 16:46:53 +0300

    added gameOver logic/small fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,78 @@
 import { useState } from 'react';
+import { WINNING_COMBINATIONS } from './winningCombinations.js';
 import Player from '@/components/Players/Player.jsx';
 import GameBoard from './components/GameBoard/GameBoard.jsx';
 import Log from './components/Log.jsx';
+import GameOver from './components/GameOver.jsx';
 import '@/App.scss';
 
+// Игровое поле([3 строки[3 ячейки]])
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+// Получение текущего активного игрока.
+function deriveActivePlayer(gameTurns) {
+  return gameTurns.length > 0 && gameTurns[0].player === 'X' ? 'O' : 'X';
+}
+// Заполнение игровой доски/проверка выигрышных комбинаций.
+function handleGameTurns(initialGameBoard, gameTurns) {
+  // Победитель партии.
+  let winner = null;
+
+  // Копия массива игрового поля(изучить как работает...)
+  const gameBoard = initialGameBoard.map((row) => [...row]);
+
+  // Проход по всем ходам игроков, изменение символов в ячейках игрового поля.
+  for (const turn of gameTurns) {
+    // Деструктуризация двух свойств из объекта turn(ход игрока).
+    const { square, player } = turn;
+    // Деструктуризация двух свойств из объекта square(координаты ячейки игрового поля).
+    const { row, col } = square;
+    // Изменение символа ячейки игрового поля.
+    gameBoard[row][col] = player;
+  }
+
+  // Проверка выигрышных комбинаций игроков(изучить подробно).
+  for (const combination of WINNING_COMBINATIONS) {
+    // Выигрышная комбинация.
+    const [
+      { row: rowA, col: colA },
+      { row: rowB, col: colB },
+      { row: rowC, col: colC },
+    ] = combination;
+    // Разбитие выигрышной комбинации на три ячейки.
+    const firstSquareSymbol = gameBoard[rowA][colA];
+    const secondSquareSymbol = gameBoard[rowB][colB];
+    const thirdSquareSymbol = gameBoard[rowC][colC];
+    // Проверка ячейки выигрышной комбинации.
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      // Определение победителя.
+      winner = firstSquareSymbol;
+      break;
+    }
+  }
+
+  // Возврат нового игрового поля и состояния победителя.
+  return { gameBoard, winner };
+}
+
 function App() {
-  // Хук состояния очереди текущего игрока(по умолчанию текущий игрок - 'X').
-  const [activePlayer, setActivePlayer] = useState('X');
   // Хук состояния логирования ходов игроков.
   const [gameTurns, setGameTurns] = useState([]);
 
   // Отслеживание и изменение очередности текущего игрока.
   function handleSelectSquare(rowIndex, colIndex) {
-    // Переключатель текущего игрока.
-    setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
     // Обработка состояния массива ходов игроков(логирование).
     setGameTurns((prevTurns) => {
       // Отслеживание текущего игрока для логирования.
-      let currentPlayer =
-        prevTurns.length > 0 && prevTurns[0].player === 'X' ? 'O' : 'X';
+      const currentPlayer = deriveActivePlayer(prevTurns);
       // Соединение информации предыдущих ходов с текушим.
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
@@ -27,6 +81,15 @@ function App() {
       return updatedTurns;
     });
   }
+
+  // Отслеживание текущего активного игрока.
+  const activePlayer = deriveActivePlayer(gameTurns);
+
+  // Заполнение игрового поля символами выбора игроков/Проверка выигрышных комбинаций.
+  const { gameBoard, winner } = handleGameTurns(initialGameBoard, gameTurns);
+
+  // Завершение партии при победе ничьей.
+  const gameOver = gameTurns.length === 9 || !!winner;
 
   return (
     <main>
@@ -49,11 +112,12 @@ function App() {
             isActive={activePlayer === 'O'}
           />
         </ol>
+        {gameOver && <GameOver winner={winner} />}
         <GameBoard
           // Смена очередности текущего игрока и передача лога ходов игроков.
           onSelectSquare={handleSelectSquare}
           // Прокидывание массива ходов игроков.
-          turns={gameTurns}
+          board={gameBoard}
         />
       </div>
       <Log turns={gameTurns} />
--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,58 +1,18 @@
 import styles from './GameBoard.module.scss';
 
-// Игровое поле([3 строки[3 ячейки]])
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({ onSelectSquare, turns }) {
-  // Копия массива игрового поля.
-  const gameBoard = [...initialGameBoard.map((innerArray) => [...innerArray])];
-
-  // Проход по всем ходам игроков, изменение символов в ячейках игрового поля.
-  for (const turn of turns) {
-    // Деструктуризация двух свойств из объекта turn(ход игрока).
-    const { square, player } = turn;
-    // Деструктуризация двух свойств из объекта square(координаты ячейки игрового поля).
-    const { row, col } = square;
-    // Изменение символа ячейки игрового поля.
-    gameBoard[row][col] = player;
-  }
-
-  /*   
-// Хук отслеживания изменения состояния игрового поля.
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-// Перерендер игрового поля при нажатии на ячейку.
-  function handleSelectSquare(rowIndex, colIndex) {
-    console.log(`${activePlayerSymbol}: [${rowIndex}][${colIndex}]`);
-    // Получение и изменение пркдыдушего состояния игрового поля.
-    setGameBoard((prevGameBoard) => {
-      // Копия предыдущего состояния игрового поля.
-      const updatedBoard = [
-        ...prevGameBoard.map((innerArray) => [...innerArray]),
-      ];
-      // Добавление символа текущего игрока на игровом поле.
-      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-      // Возврат измененного состояния поля обратно в хук gameBoard.
-      return updatedBoard;
-    });
-    // Изменение текущего игрока.
-    onSelectSquare();
-  } 
-*/
-
+export default function GameBoard({ onSelectSquare, board }) {
   return (
     <div className={styles.root}>
       <ol>
-        {gameBoard.map((row, rowIndex) => (
+        {board.map((row, rowIndex) => (
           <li key={rowIndex}>
             <ol>
               {row.map((playerSymbol, colIndex) => (
                 <li key={colIndex}>
-                  <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                  <button
+                    onClick={() => onSelectSquare(rowIndex, colIndex)}
+                    disabled={playerSymbol !== null}
+                  >
                     {playerSymbol}
                   </button>
                 </li>
